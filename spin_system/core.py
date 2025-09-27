dc2c800ea90c47ff74f1134e4456db6f5cbace84
Corbanez97 <lucascorbanez@gmail.com>
2025-09-27 17:38:07 -0300

    Refactor SpinSystem to work with lattice_replicas and evolve them separately through the metropolis algorithm; MISC: Notebook with testing population metropolis

--- spin_system/core.py
+++ spin_system/core.py
@@ -8,6 +8,7 @@ class SpinSystem(tf.Module):
         self,
         lattice_dim: int,
         lattice_length: int,
+        lattice_replicas: int,
         interaction_matrix: Union[tf.Tensor, np.ndarray],
         initial_spin_state: Optional[Union[tf.Tensor, np.ndarray]] = None,
         external_field: Optional[Union[tf.Tensor, np.ndarray]] = None,
@@ -16,11 +17,16 @@ class SpinSystem(tf.Module):
         initial_magnetization: float = 0.5,
     ) -> None:
 
+        assert model in ("ising", "spherical", "z2_gauge"), \
+            ValueError(
+                f"Invalid model: {model}. Please select one of 'ising', 'spherical' or 'z2_gauge'.")
+
         assert not (model != "spherical" and spherical_constraint), \
             "Spherical constraint can only be applied to spherical model!"
 
         self.lattice_dim = lattice_dim
         self.lattice_length = lattice_length
+        self.lattice_replicas = lattice_replicas
         self.shape = [lattice_length] * lattice_dim
         self.number_spins = tf.cast(lattice_length ** lattice_dim, tf.float32)
         self.model = model
@@ -43,7 +49,7 @@ class SpinSystem(tf.Module):
 
         self.spin_state = self._validate_tensor_shape(
             initial_spin_state,
-            expected_shape=self.shape,
+            expected_shape=[self.shape] * self.lattice_replicas,
             name="Initial spin state",
             allow_none=True,
             default=lambda: tf.Variable(
@@ -51,7 +57,8 @@ class SpinSystem(tf.Module):
             ),
         )
 
-        self.energy = self.compute_pairwise_energy()
+        self.energy_replicas = self.compute_pairwise_energies()
+        self.energy_average = tf.reduce_mean(self.energy_replicas)
 
     def _validate_tensor_shape(
         self,
@@ -85,7 +92,19 @@ class SpinSystem(tf.Module):
 
     @tf.function
     def _apply_spherical_constraint(self, spin_state: tf.Tensor) -> tf.Tensor:
-        return tf.sqrt(self.number_spins) * tf.math.l2_normalize(spin_state)
+        """
+        Applies the spherical constraint independently to each replica.
+        """
+        original_shape = tf.shape(spin_state)
+        spin_state_flat_replicas = tf.reshape(
+            spin_state, (self.lattice_replicas, -1))
+
+        normalized_flat = tf.math.l2_normalize(
+            spin_state_flat_replicas, axis=1)
+
+        normalized_spins = tf.reshape(normalized_flat, original_shape)
+
+        return tf.sqrt(self.number_spins) * normalized_spins
 
     def _initialize_spins_state(self) -> tf.Tensor:
         if self.model == "ising":
@@ -97,14 +116,22 @@ class SpinSystem(tf.Module):
             # TODO: We could implement an argument that enables a different initial distribution for spins
             spin_state = tf.random.normal(
                 self.shape, mean=self.initial_magnetization, stddev=1.0)
-            if self.spherical_constraint:
-                spin_state = self._apply_spherical_constraint(spin_state)
         elif self.model == "z2_gauge":
             raise NotImplementedError("Z2 gauge model not implemented yet")
+
+        expanded_spin_state = tf.expand_dims(spin_state, axis=0)
+
+        multiples = tf.constant(
+            [self.lattice_replicas] + [1] * self.lattice_dim, dtype=tf.int32)
+
+        spin_state = tf.tile(expanded_spin_state, multiples)
+        if self.spherical_constraint:
+            spin_state = self._apply_spherical_constraint(spin_state)
+
         return spin_state
 
     @tf.function
-    def compute_pairwise_energy(
+    def compute_pairwise_energies(
         self,
         spin_state: Optional[tf.Tensor] = None,
         interaction_matrix: Optional[tf.Tensor] = None,
@@ -120,59 +147,71 @@ class SpinSystem(tf.Module):
         if external_field is None:
             external_field = self.external_field
 
-        spin_state_flat = tf.reshape(spin_state, (1, self.number_spins))
+        spin_state_flat = tf.reshape(spin_state, (self.lattice_replicas, -1))
+
         interaction_matrix_flat = tf.reshape(
             interaction_matrix, (self.number_spins, self.number_spins))
+
         external_field_flat = tf.reshape(
-            external_field, (1, self.number_spins))
+            external_field, (1, -1))
+        h = spin_state_flat @ interaction_matrix_flat
+        pairwise = -0.5 * tf.reduce_sum(spin_state_flat * h, axis=1)
+        field_term = -tf.reduce_sum(spin_state_flat *
+                                    external_field_flat, axis=1)
 
-        pairwise = -0.5 * \
-            (spin_state_flat @ interaction_matrix_flat @
-             tf.transpose(spin_state_flat))
-        field_term = - external_field_flat @ tf.transpose(spin_state_flat)
-
-        return tf.squeeze(pairwise + field_term)
+        return pairwise + field_term
 
     @tf.function
-    def _compute_pairwise_energy_delta(
+    def _compute_pairwise_energy_deltas(
         self,
         spin_flat: tf.Tensor,
         updated_spin_flat: tf.Tensor,
-        disturbed_idx: tf.Tensor,
+        disturbed_idx: tf.Tensor
     ) -> tf.Tensor:
 
-        # \Delta \sigma = \sigma_new - \sigma_old
-        delta_sigma = tf.gather(updated_spin_flat - spin_flat, disturbed_idx)
+        replicas = tf.shape(spin_flat)[0]
+        num_flips = tf.shape(disturbed_idx)[1]
+        num_spins = tf.cast(self.number_spins, tf.int32)
+
+        batch_idx = tf.repeat(tf.range(replicas)[:, None], num_flips, axis=1)
+        gather_indices = tf.stack([batch_idx, disturbed_idx], axis=-1)
+        delta_sigma = tf.gather_nd(
+            updated_spin_flat - spin_flat, gather_indices)
 
         interaction_matrix_flat = tf.reshape(
-            self.interaction_matrix, (self.number_spins, self.number_spins))
+            self.interaction_matrix, (num_spins, num_spins))
 
-        # local fields h_j = \sum_i J_ij \sigma_i
-        h = tf.linalg.matvec(interaction_matrix_flat, spin_flat)
+        h = tf.linalg.matmul(
+            spin_flat, interaction_matrix_flat)
 
-        # interaction contributions
-        # - \sum_j \Delta \sigma_j h_j
-        term1 = -tf.reduce_sum(delta_sigma * tf.gather(h, disturbed_idx))
+        h_j = tf.gather(h, disturbed_idx, batch_dims=1)
+        term1 = -tf.reduce_sum(delta_sigma * h_j, axis=1)
 
-        # -1/2 \sum_{i,j \in \text{disturbed spins}} J_ij \Delta \sigma_i \Delta \sigma_j
-        J_sub = tf.gather(
-            tf.gather(interaction_matrix_flat, disturbed_idx, axis=0),
-            disturbed_idx,
-            axis=1,
-        )
-        term2 = -0.5 * tf.tensordot(
-            delta_sigma, tf.linalg.matvec(J_sub, delta_sigma), axes=1
-        )
+        J_rows = tf.gather(interaction_matrix_flat, disturbed_idx, axis=0)
 
-        # external field contribution: - \sum_j \Delta \sigma_j h_ext_j
-        field_term = -tf.reduce_sum(
-            delta_sigma *
-            tf.gather(tf.reshape(self.external_field, [-1]), disturbed_idx)
-        )
+        J_sub = tf.gather(J_rows, disturbed_idx, axis=2, batch_dims=1)
+
+        delta_sigma_expanded = tf.expand_dims(
+            delta_sigma, axis=1)
+        quad_form = tf.matmul(delta_sigma_expanded, tf.matmul(
+            J_sub, tf.expand_dims(delta_sigma, axis=-1)))
+        term2 = -0.5 * tf.squeeze(quad_form, axis=[1, 2])
+
+        field_flat = tf.reshape(self.external_field, [-1])
+
+        disturbed_fields = tf.gather(field_flat, disturbed_idx)
+
+        field_term = -tf.reduce_sum(delta_sigma * disturbed_fields, axis=1)
 
         return term1 + term2 + field_term
 
     # TODO: Compute the magnetization susceptibility as the variance of the magnetization. I need to first use a populational method to compute the mag
+
+    @tf.function
+    def compute_magnetizations(self, spin_state: Optional[tf.Tensor] = None) -> tf.Tensor:
+        if spin_state is None:
+            spin_state = self.spin_state
+        return tf.reduce_mean(tf.reshape(spin_state, (self.lattice_replicas, -1)), axis=1)
 
     @tf.function
     def compute_magnetization(self, spin_state: Optional[tf.Tensor] = None) -> tf.Tensor:
@@ -181,8 +220,14 @@ class SpinSystem(tf.Module):
         return tf.reduce_mean(spin_state)
 
     @tf.function
+    def compute_magnetic_susceptibility(self, spin_state: Optional[tf.Tensor] = None) -> tf.Tensor:
+        if spin_state is None:
+            spin_state = self.spin_state
+        return tf.math.reduce_variance(spin_state)
+
+    @tf.function
     def flip_spins(self, num_flips: tf.Tensor, spin_state: Optional[tf.Tensor] = None, spin_flat: Optional[tf.Tensor] = None) -> Tuple[tf.Tensor, tf.Tensor]:
-        """Flip `num_flips` random spins in Ising model."""
+        """Flip `num_flips` random spins in replicas of Ising model."""
         both_none = (spin_state is None) and (spin_flat is None)
         exactly_one = (spin_state is not None) ^ (spin_flat is not None)
 
@@ -190,59 +235,92 @@ class SpinSystem(tf.Module):
                                   message="Either provide no arguments or exactly one of spin_state or spin_flat")
 
         if spin_state is not None:
-            spin_flat = tf.reshape(spin_state, [-1])
+            spin_flat = tf.reshape(spin_state, (self.lattice_replicas, -1))
         elif spin_flat is None:
-            spin_flat = tf.reshape(self.spin_state, [-1])
+            spin_flat = tf.reshape(
+                self.spin_state, (self.lattice_replicas, -1))
 
-        idx = tf.random.shuffle(tf.range(self.number_spins, dtype=tf.int32))[
-            :num_flips]
-        updates = -tf.gather(spin_flat, idx)
-        updated = tf.tensor_scatter_nd_update(spin_flat,
-                                              indices=tf.expand_dims(idx, 1),
-                                              updates=updates)
-        energy_delta = self._compute_pairwise_energy_delta(
+        idx = tf.stack([
+            tf.random.shuffle(tf.range(self.number_spins, dtype=tf.int32))[
+                :num_flips]
+            for _ in range(self.lattice_replicas)
+        ], axis=0)
+        replica_idx = tf.repeat(tf.range(self.lattice_replicas)[
+                                :, None], num_flips, axis=1)
+        scatter_indices = tf.stack([replica_idx, idx], axis=-1)
+        scatter_indices = tf.reshape(scatter_indices, (-1, 2))
+
+        updates = tf.reshape(
+            -tf.gather_nd(spin_flat, scatter_indices),
+            (self.lattice_replicas * num_flips,)
+        )
+
+        updated = tf.tensor_scatter_nd_update(
+            spin_flat, scatter_indices, updates)
+
+        energy_delta = self._compute_pairwise_energy_deltas(
             spin_flat, updated, idx)
+
         return updated, energy_delta
 
     @tf.function
     def rotate_spins(self, num_pairs: tf.Tensor, theta_max: tf.Tensor,
                      spin_state: Optional[tf.Tensor] = None, spin_flat: Optional[tf.Tensor] = None) -> Tuple[tf.Tensor, tf.Tensor]:
-        """Rotate `num_pairs` pairs of spins by random angles in [-theta_max, theta_max]."""
+        """Rotate `num_pairs` pairs of spins by random angles in [-theta_max, theta_max] for each replica."""
 
         both_none = (spin_state is None) and (spin_flat is None)
-        exactly_one = (spin_state is not None) ^ (
-            spin_flat is not None)
+        exactly_one = (spin_state is not None) ^ (spin_flat is not None)
 
         tf.debugging.assert_equal(both_none or exactly_one, True,
                                   message="Either provide no arguments or exactly one of spin_state or spin_flat")
 
         if spin_state is not None:
-            spin_flat = tf.reshape(spin_state, [-1])
+            spin_flat = tf.reshape(spin_state, (self.lattice_replicas, -1))
         elif spin_flat is None:
-            spin_flat = tf.reshape(self.spin_state, [-1])
-        # num_pairs = tf.cast(num_pairs, tf.int32)
-        idx = tf.random.shuffle(tf.range(self.number_spins, dtype=tf.int32))[
-            : 2 * num_pairs]
-        idx1, idx2 = idx[:num_pairs], idx[num_pairs:]
+            spin_flat = tf.reshape(
+                self.spin_state, (self.lattice_replicas, -1))
 
-        sigma_i = tf.gather(spin_flat, idx1)
-        sigma_j = tf.gather(spin_flat, idx2)
+        num_spins = tf.cast(self.number_spins, tf.int32)
 
-        theta = tf.random.uniform([num_pairs], -theta_max, theta_max)
+        all_indices = tf.stack([
+            tf.random.shuffle(tf.range(num_spins))[:2 * num_pairs]
+            for _ in range(self.lattice_replicas)
+        ], axis=0)
+
+        idx1 = all_indices[:, :num_pairs]
+        idx2 = all_indices[:, num_pairs:]
+
+        replica_idx = tf.repeat(tf.range(self.lattice_replicas)[
+                                :, None], num_pairs, axis=1)
+
+        gather_indices_i = tf.stack([replica_idx, idx1], axis=-1)
+        gather_indices_j = tf.stack([replica_idx, idx2], axis=-1)
+
+        sigma_i = tf.gather_nd(spin_flat, gather_indices_i)
+        sigma_j = tf.gather_nd(spin_flat, gather_indices_j)
+
+        theta = tf.random.uniform(
+            [self.lattice_replicas, num_pairs], -theta_max, theta_max)
         cos_t, sin_t = tf.cos(theta), tf.sin(theta)
 
         new_i = cos_t * sigma_i - sin_t * sigma_j
         new_j = sin_t * sigma_i + cos_t * sigma_j
 
-        updates = tf.concat([new_i, new_j], axis=0)
-        indices = tf.concat([tf.expand_dims(idx1, 1),
-                            tf.expand_dims(idx2, 1)], axis=0)
-        updated = tf.tensor_scatter_nd_update(spin_flat, indices, updates)
+        updates = tf.concat([new_i, new_j], axis=1)
+        disturbed_idx = tf.concat([idx1, idx2], axis=1)
 
-        energy_delta = self._compute_pairwise_energy_delta(
-            spin_flat, updated, idx)
+        scatter_replica_idx = tf.repeat(tf.range(self.lattice_replicas)[
+                                        :, None], 2 * num_pairs, axis=1)
+        scatter_indices = tf.stack(
+            [scatter_replica_idx, disturbed_idx], axis=-1)
 
-        return updated, energy_delta
+        updated_flat = tf.tensor_scatter_nd_update(
+            spin_flat, scatter_indices, updates)
+
+        energy_delta = self._compute_pairwise_energy_deltas(
+            spin_flat, updated_flat, disturbed_idx)
+
+        return updated_flat, energy_delta
 
     @tf.function
     def _disturb_state(self, num_disturb: Optional[tf.Tensor], theta_max: Optional[tf.Tensor]) -> Tuple[tf.Tensor, tf.Tensor]:
@@ -263,21 +341,39 @@ class SpinSystem(tf.Module):
         return next_spin_state, energy_delta
 
     @tf.function
-    def metropolis_step(self, beta: float, num_disturb: Optional[tf.Tensor] = 1, theta_max: Optional[tf.Tensor] = None) -> tf.Tensor:
+    def metropolis_step(
+        self,
+        beta: float,
+        num_disturb: Optional[tf.Tensor] = 1,
+        theta_max: Optional[tf.Tensor] = None
+    ) -> tf.Tensor:
+        """
+        Perform a single Metropolis update step independently for each replica.
+        """
         next_spin_state, energy_delta = self._disturb_state(
             num_disturb=num_disturb,
             theta_max=theta_max
         )
 
-        accept = tf.logical_or(
-            energy_delta < 0.0,
-            tf.random.uniform([]) < tf.exp(-beta * energy_delta)
+        prob_accept = tf.exp(-beta * energy_delta)
+
+        random_vals = tf.random.uniform(
+            shape=(self.lattice_replicas,), dtype=tf.float32
         )
 
-        # new_spin_state = tf.where(accept, next_spin_state, self.spin_state)
+        accept = tf.logical_or(
+            energy_delta < 0.0,
+            random_vals < prob_accept
+        )
 
-        if accept:
-            self.spin_state.assign(next_spin_state)
+        if tf.reduce_any(accept):
+
+            new_spin_state = tf.where(
+                tf.reshape(accept, (-1,) + (1,) * self.lattice_dim),
+                next_spin_state,
+                self.spin_state
+            )
+            self.spin_state.assign(new_spin_state)
 
         return self.spin_state
 
@@ -287,15 +383,22 @@ class SpinSystem(tf.Module):
         beta: float,
         num_disturb: Optional[tf.Tensor] = 1,
         theta_max: Optional[tf.Tensor] = None,
-        sweep_length: int = 100,
+        sweep_length: int = None,
         measurement_granularity: int = 100,
         track_spins: bool = True,
         track_energy: bool = True,
         track_magnetization: bool = True,
     ) -> Dict:
+
+        assert not (self.model == "spherical" and theta_max is None), \
+            "For the spherical model, theta_max must be provided."
+
         def make_array(track, size):
             # This is bad because the code tries to access a tensor even if I don't want to track it...
             return tf.TensorArray(dtype=tf.float32, size=size) if track else tf.TensorArray(dtype=tf.float32, size=0)
+
+        if sweep_length == None:
+            sweep_length = self.number_spins * measurement_granularity
 
         spin_evolution = make_array(
             track_spins, int(round(sweep_length/measurement_granularity)) + 1)
@@ -308,10 +411,10 @@ class SpinSystem(tf.Module):
             spin_evolution = spin_evolution.write(0, self.spin_state)
         if track_energy:
             energy_evolution = energy_evolution.write(
-                0, self.compute_pairwise_energy())
+                0, self.compute_pairwise_energies())
         if track_magnetization:
             magnetization_evolution = magnetization_evolution.write(
-                0, self.compute_magnetization())
+                0, self.compute_magnetizations())
 
         def body(i, spin_evolution, energy_evolution, magnetization_evolution):
             _ = self.metropolis_step(beta, num_disturb, theta_max)
@@ -323,10 +426,10 @@ class SpinSystem(tf.Module):
                         j + 1, self.spin_state)
                 if track_energy:
                     energy_evolution = energy_evolution.write(
-                        j + 1, self.compute_pairwise_energy())
+                        j + 1, self.compute_pairwise_energies())
                 if track_magnetization:
                     magnetization_evolution = magnetization_evolution.write(
-                        j + 1, self.compute_magnetization())
+                        j + 1, self.compute_magnetizations())
 
             return i + 1, spin_evolution, energy_evolution, magnetization_evolution
 
